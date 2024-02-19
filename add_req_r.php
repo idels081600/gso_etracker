c038d240ad2b046743ef2b5ea8e9bf3dae44bbbe
idels081600 <laureanobryan07@gmail.com>
2024-02-19 09:26:37 +0800

    updated

--- add_req_r.php
+++ add_req_r.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['username'])) {
 } else if ($_SESSION['role'] == 'Desk Clerk') {
     header("location:login_v2.php");
 }
+$conn = mysqli_connect("localhost", "bryanmysql", 'gsotagbilaran', "my_data");
 
 if (isset($_POST['save_data2'])) {
     $name = $_POST['name'];
