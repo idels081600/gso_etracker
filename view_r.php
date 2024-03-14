269bc92feb745a74f0131b0633a9bdf22ee2c796
idels081600 <laureanobryan07@gmail.com>
2024-03-14 11:06:22 +0800

    Update view_r.php

--- view_r.php
+++ view_r.php
@@ -26,8 +26,7 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -90,13 +89,12 @@ if (!isset($_SESSION['username'])) {
             <img src="logo.png" alt="Logo" class="logo-img">
             <span class="logo-text">E-Pass </span>
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="nav navbar-nav navbar-right">
+            <ul class="nav navbar-nav navbar-right">
                 <li class="nav-item">
                     <a class="nav-link" href="index_r.php">Home <span class="sr-only">(current)</span></a>
                 </li>
@@ -161,7 +159,7 @@ if (!isset($_SESSION['username'])) {
 
                         if (mysqli_num_rows($query_run) > 0) {
                             $data = mysqli_fetch_array($query_run);
-                            ?>
+                    ?>
                             <form action="code.php" method="POST">
                                 <div class="container">
                                     <input type="hidden" name="data_id" value="<?= $data['id']; ?>">
@@ -216,7 +214,7 @@ if (!isset($_SESSION['username'])) {
                                         <div class="form-group">
                                             <label for="sel1">Status:</label>
                                             <select class="form" id="sel1" name='status'>
-                                               
+
                                                 <option>Partially Approved</option>
                                                 <option>Declined</option>
                                             </select>
@@ -245,7 +243,7 @@ if (!isset($_SESSION['username'])) {
                                     </div>
                                 </div>
                             </form>
-                            <?php
+                    <?php
                         } else {
                             echo "<h4>No Such Id Found</h4>";
                         }
@@ -262,7 +260,7 @@ if (!isset($_SESSION['username'])) {
     </div>
     <script>
         // Add an event listener to the dropdown
-        document.getElementById("sel1").addEventListener("change", function () {
+        document.getElementById("sel1").addEventListener("change", function() {
             var selectedStatus = this.value;
             var reasonDiv = document.getElementById("reason");
 
@@ -276,7 +274,7 @@ if (!isset($_SESSION['username'])) {
     </script>
     <script>
         // Add an event listener to the dropdown
-        document.getElementById("sel1").addEventListener("change", function () {
+        document.getElementById("sel1").addEventListener("change", function() {
             var selectedStatus = this.value;
             var reasonDiv = document.getElementById("declineButtonContainer");
 
@@ -288,10 +286,33 @@ if (!isset($_SESSION['username'])) {
             }
         });
     </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var statusSelect = document.getElementById('sel1');
+            var declineButtonContainer = document.getElementById('declineButtonContainer');
+
+            // Function to toggle visibility of decline button based on selected option
+            function toggleDeclineButtonVisibility() {
+                if (statusSelect.value === 'Partially Approved') {
+                    declineButtonContainer.style.display = 'none';
+                } else {
+                    declineButtonContainer.style.display = 'block';
+                }
+            }
+
+            // Initially call the function to set initial visibility
+            toggleDeclineButtonVisibility();
+
+            // Add change event listener to the status select element
+            statusSelect.addEventListener('change', function() {
+                toggleDeclineButtonVisibility();
+            });
+        });
+    </script>
 
     <script>
         // Add an event listener to the dropdown
-        document.getElementById("sel1").addEventListener("change", function () {
+        document.getElementById("sel1").addEventListener("change", function() {
             var selectedStatus = this.value;
             var approveButtonContainer = document.getElementById("approveButtonContainer");
 
@@ -304,13 +325,9 @@ if (!isset($_SESSION['username'])) {
         });
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 
 </html>
