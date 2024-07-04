81ee15d66871741e233944c99fe14257e0b07e71
idels081600 <laureanobryan07@gmail.com>
2024-07-04 14:51:04 +0800

    Update tracking.php

--- tracking.php
+++ tracking.php
@@ -44,7 +44,7 @@ if (isset($_POST['save_data'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customizable Sidebar</title>
+    <title>Tent Tracker</title>
     <link rel="stylesheet" href="sidebar_asset.css">
     <link rel="stylesheet" href="tracking_style.css">
     <link rel="stylesheet" href="style_box.css">
