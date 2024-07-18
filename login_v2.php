0ac134e2a26662e2a04bd33d6084c0e79a30e34a
idels081600 <laureanobryan07@gmail.com>
2024-07-18 14:28:08 +0800

    Update login_v2.php

--- login_v2.php
+++ login_v2.php
@@ -229,7 +229,7 @@ session_start();
       }
     }
   </style>
-  <link rel="canonical" href="https://gso.etracker.tagbilaran.gov.ph/login_v2.php">
+  <link rel="canonical" href="https://gso.etracker.tagbilaran.gov.ph/login_v2">
 </head>
 <nav class="navbar navbar-expand-lg fixed-top">
   <div class="container">
