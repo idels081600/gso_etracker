351956607781e287a09d6bf38486fd3d50dfe742
idels081600 <laureanobryan07@gmail.com>
2025-03-31 11:59:39 +0800

    Update delete_print_bq.php

--- delete_print_bq.php
+++ delete_print_bq.php
@@ -1,13 +1,8 @@
 <?php
 require_once 'db.php';
 
-if(isset($_POST['id'])) {
-    $id = intval($_POST['id']);
-    $query = "DELETE FROM bq_print WHERE id = $id";
-    $result = mysqli_query($conn, $query);
-    
-    echo json_encode(['success' => $result]);
-} else {
-    echo json_encode(['success' => false]);
-}
+$query = "DELETE FROM bq_print";
+$result = mysqli_query($conn, $query);
+
+echo json_encode(['success' => $result]);
 ?>
