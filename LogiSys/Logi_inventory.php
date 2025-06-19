92844ecf815d4202dcb8a898bdb3bc795ee341f9
idels081600 <laureanobryan07@gmail.com>
2025-06-19 10:50:46 +0800

    Update Logi_inventory.php

--- LogiSys/Logi_inventory.php
+++ LogiSys/Logi_inventory.php
@@ -617,7 +617,7 @@ function getStatusBadge($status)
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="Logi_Sys_inventory.js"></script>
+    <script src="LogiSys/Logi_Sys_inventory.js"></script>
 </body>
 
 </html>
