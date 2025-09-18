100c8294b7c08d00eb8b3de40d93242b2fdc6035
idels081600 <laureanobryan07@gmail.com>
2025-09-18 08:02:12 +0800

    Update export_r.php

--- export_r.php
+++ export_r.php
@@ -11,11 +11,11 @@ if (!isset($_SESSION['username'])) {
 }
  
 // Query for Official Business
-$result_official = "SELECT * FROM request WHERE Status = 'Done' AND TypeofBusiness = 'Official Business' AND confirmed_by = 'CAGULADA RENE ART' ORDER BY id";
+$result_official = "SELECT * FROM request WHERE Status = 'Done' AND TypeofBusiness = 'Official Business' AND confirmed_by = 'CAGULADA RENE ART' AND confirmed_by = 'Pahang Dave' AND role = 'Employee' ORDER BY id";
 $sql_official = $conn->query($result_official);
 
 // Query for Personal Business
-$result_personal = "SELECT * FROM request WHERE Status = 'Done' AND TypeofBusiness = 'Personal' AND confirmed_by = 'CAGULADA RENE ART' ORDER BY id";
+$result_personal = "SELECT * FROM request WHERE Status = 'Done' AND TypeofBusiness = 'Personal' AND confirmed_by = 'CAGULADA RENE ART' AND confirmed_by = 'Pahang Dave' AND role = 'Employee' ORDER BY id";
 $sql_personal = $conn->query($result_personal);
 
 
