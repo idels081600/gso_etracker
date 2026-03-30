6d1c3ed76d202b9807f143407d586282038e1252
idels081600 <laureanobryan07@gmail.com>
2026-03-30 10:59:12 +0800

    secure db

--- document_tracker/asset_db.php
+++ document_tracker/asset_db.php
@@ -1,8 +1,18 @@
 <?php
-$servername = "157.245.193.124";
-$username = "bryanmysql";
-$password = "gsotagbilaran";
-$dbname = "docu_tracker";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$envPath = __DIR__ . '/.env';
+if (!file_exists($envPath)) {
+    die('Missing .env file. Copy .env.example to .env and set your database credentials.');
+}
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
 
 // Attempt to establish a connection to the database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -16,4 +26,3 @@ if (!$conn) {
 
 // Set the character set
 $conn->set_charset("utf8mb4");
- 
